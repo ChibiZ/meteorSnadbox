a416a840393af65122d367bdc01d44eb26c8f59a
Aleksey Kozlov <Aleksey.Kozlov1@x5.ru>
2024-10-02 17:22:04 +0300

    init

---
+++ imports/ui/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Hello } from './Hello.jsx';
+import { Info } from './Info.jsx';
+import { Flow } from './Flow.jsx';
+
+export const App = () => (
+  <div>
+    <Hello/>
+    <Info/>
+    <Flow/>
+  </div>
+);
