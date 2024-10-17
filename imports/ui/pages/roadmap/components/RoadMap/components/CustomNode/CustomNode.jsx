974ad61435d7e66c57897a4570965b34666852c1
Mazitov, Dmitry <dmitry.mazitov@x5.ru>
2024-10-17 21:27:40 +0300

    min fix

--- imports/ui/pages/roadmap/components/RoadMap/components/CustomNode/CustomNode.jsx
+++ imports/ui/pages/roadmap/components/RoadMap/components/CustomNode/CustomNode.jsx
@@ -1,69 +1,71 @@
 import React, { memo } from 'react';
 import { Handle, Position } from '@xyflow/react';
 
-export const CustomNode = memo(({ data, isConnectable, draggable }) => {
-  const style = draggable ? undefined : { opacity: 0 };
+export const CustomNode = memo(
+  ({ data, isConnectable, draggable, ...props }) => {
+    const style = draggable ? undefined : { opacity: 0 };
 
-  return (
-    <div className={`node ${data.kind} ${data.status ?? ''}`}>
-      <div>{data.label}</div>
+    return (
+      <div className={`node ${data.kind} ${data.status ?? ''}`}>
+        <div>{data.label}</div>
 
-      <Handle
-        type="target"
-        position={Position.Left}
-        id="y1"
-        isConnectable={isConnectable}
-        style={style}
-      />
-      <Handle
-        type="source"
-        position={Position.Left}
-        id="y2"
-        isConnectable={isConnectable}
-        style={style}
-      />
-      <Handle
-        type="target"
-        position={Position.Bottom}
-        id="x1"
-        isConnectable={isConnectable}
-        style={style}
-      />
-      <Handle
-        type="source"
-        position={Position.Bottom}
-        id="x2"
-        isConnectable={isConnectable}
-        style={style}
-      />
-      <Handle
-        type="target"
-        position={Position.Right}
-        id="z1"
-        isConnectable={isConnectable}
-        style={style}
-      />
-      <Handle
-        type="source"
-        position={Position.Right}
-        id="z2"
-        isConnectable={isConnectable}
-        style={style}
-      />
-      <Handle
-        type="target"
-        position={Position.Top}
-        id="w1"
-        isConnectable={isConnectable}
-        style={style}
-      />
-      <Handle
-        type="source"
-        position={Position.Top}
-        id="w2"
-        isConnectable={isConnectable}
-        style={style}
-      />
-    </div>
-  );
-});
+        <Handle
+          type="target"
+          position={Position.Left}
+          id="y1"
+          isConnectable={isConnectable}
+          style={style}
+        />
+        <Handle
+          type="source"
+          position={Position.Left}
+          id="y2"
+          isConnectable={isConnectable}
+          style={style}
+        />
+        <Handle
+          type="target"
+          position={Position.Bottom}
+          id="x1"
+          isConnectable={isConnectable}
+          style={style}
+        />
+        <Handle
+          type="source"
+          position={Position.Bottom}
+          id="x2"
+          isConnectable={isConnectable}
+          style={style}
+        />
+        <Handle
+          type="target"
+          position={Position.Right}
+          id="z1"
+          isConnectable={isConnectable}
+          style={style}
+        />
+        <Handle
+          type="source"
+          position={Position.Right}
+          id="z2"
+          isConnectable={isConnectable}
+          style={style}
+        />
+        <Handle
+          type="target"
+          position={Position.Top}
+          id="w1"
+          isConnectable={isConnectable}
+          style={style}
+        />
+        <Handle
+          type="source"
+          position={Position.Top}
+          id="w2"
+          isConnectable={isConnectable}
+          style={style}
+        />
+      </div>
+    );
+  },
+);
