28c667f68a1cedbba740a9ac6ea1f0c7f7148148
Aleksey <Aleksey.Kozlov1@x5.ru>
2024-12-05 16:37:09 +0300

    Update const.js

--- imports/ui/pages/roadmap/components/AdminAIEditor/const.js
+++ imports/ui/pages/roadmap/components/AdminAIEditor/const.js
@@ -1,2 +1,2 @@
 export const url =
-  'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyC8jaqHygUS2DYw4YWQO_9k1fM9UvPYf3w';
+  'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyBlGX8u4AD-asheucrp3wqSLR5f4p3pZT0';
